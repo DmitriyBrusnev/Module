16f6bf0570617a3e61cab33d7ceb70d2c6654876
Dmitry Brusnev <dmitry.brusnev@kidskey.org>
2021-08-18 10:32:34 +0300

    task5: ready to test

--- task5/main.js
+++ task5/main.js
@@ -15,11 +15,10 @@ function range(min, max) {
         result = BigInt(result)
         min = BigInt(min);
         max = BigInt(max);
-        // if (min > max) {
-        //     [min, max] = [max, min];
-        // }
+        if (min > max) {
+            [min, max] = [max, min];
+        }
         for (let i = min + 1n; i < max + 1n; i += 1n) {
-            console.log(i);
             result += BigInt(i);
         }
     } else {
@@ -29,7 +28,7 @@ function range(min, max) {
         min = Number(min);
         max = Number(max);
         if (isNaN(min) || isNaN(max)) throw new Error('min & max must be numbers');
-        result = (max + min) / (2n) * (max - min + 1n);
+        result = (max + min) / (2) * (max - min + 1);
     }
     return result;
 }
@@ -37,9 +36,15 @@ function range(min, max) {
 const rangeMemo = memoize(range);
 
 console.time('test 1');
-console.log(rangeMemo(Number.MAX_SAFE_INTEGER + 10, Number.MAX_SAFE_INTEGER = 11));
+console.log(rangeMemo(Number.MAX_SAFE_INTEGER + 10, Number.MAX_SAFE_INTEGER + 14));
 console.timeEnd('test 1');
-
 console.time('test 2');
-console.log(rangeMemo(5, 7));
+console.log(rangeMemo(Number.MAX_SAFE_INTEGER + 10, Number.MAX_SAFE_INTEGER + 14));
 console.timeEnd('test 2');
+
+console.time('test 3');
+console.log(rangeMemo(5, 7));
+console.timeEnd('test 3');
+console.time('test 4');
+console.log(rangeMemo(5, 7));
+console.timeEnd('test 4');
